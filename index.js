55b5c63264ac3c8bb4278563899e006fdfce960a
Jose Germán Martínez <jgermanmm@gmail.com>
2023-07-13 15:58:57 -0400

    definicion inicial de rutas

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const app = express();
 const conn = require("./services/db");
+const userModel = require("./models/user")
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+
+//RUTAS
+app.post("/create", )
+app.get("/user", )
 
 const port = 3001;
 
@@ -11,12 +17,13 @@ const database = async () => {
   try {
     await conn.authenticate();
     console.log("Base de datos conectada");
+    await userModel.sync({force:true});
   } catch (error) {
     console.log("Algo salió mal en la conexión de la BD", error);
   }
 };
 
 app.listen(port, () => {
-    database();
-    console.log("Servidor ejecutandose correctamente")
+  database();
+  console.log("Servidor ejecutandose correctamente");
 });
