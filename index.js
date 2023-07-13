a8eb3bb7f3569aac1455b4ba833964c34bf8eb66
Jose Germán Martínez <jgermanmm@gmail.com>
2023-07-13 15:26:13 -0400

    inicio de app

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const conn = require("./services/db");
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 const port = 3001;
 
-app.listen(port, console.log("Servidor ejecutandose correctamente"));
+const database = async () => {
+  try {
+    await conn.authenticate();
+    console.log("Base de datos conectada");
+  } catch (error) {
+    console.log("Algo salió mal en la conexión de la BD", error);
+  }
+};
+
+app.listen(port, () => {
+    database();
+    console.log("Servidor ejecutandose correctamente")
+});
--- models/user.js
+++ models/user.js
@@ -1,21 +1,65 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
+const dataBase = require("../services/db")
+
 
 const Users = dataBase.define(
-    "Users",
-    {
-        idUser: {
-            type:DataTypes.UUID,
-            defaultValue: DataTypes.UUIDV4, // 36 caracteres 
-            primaryKey: true,
-            allowNull: false
+  "Users",
+  {
+    idUser: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4, // 36 caracteres
+      primaryKey: true,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true, // email@email.com tiene este formato
+        notNull: {
+          msg: "El correo es obligatorio",
+        },
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: "La contraseña es obligatoria",
+        },
+      },
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: "El nombre es obligatoria",
+        },
+      },
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: "El apellido es obligatorio",
+        },
+      },
+    },
+    status: {
+      type: DataTypes.ENUM("ACTIVE", "BLOCK", "DELETE", "PENDING"),
+      allowNull: false,
+      defaultValue: "ACTIVE",
+    },
+  },
+  {
+    timestamps: true,
+    createdAt: true,
+    updateAt: true,
+  }
+);
 
-        }
-
-
-
-
-
-
-
-    }
-)
+module.exports = Users;
