7abe0dbc6c041efa51da60fc7a5099da65156dc6
Jose Germán Martínez <jgermanmm@gmail.com>
2023-07-17 15:18:52 -0400

    cambio en la declaración del puerto

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const app = require("./src/app/app");
 const users = require("./src/models/user.models");
+const express = require("express");
 
 const port = process.env.PORT || 8080;
 
 users.sync();
 
-app.listen(port, () => {
-  console.log("Servidor ejecutandose correctamente");
+const server = app.listen(port, () => {
+  console.log("Servidor ejecutandose correctamente", server.address().port);
 });
